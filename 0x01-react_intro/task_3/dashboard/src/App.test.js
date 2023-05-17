7d829a379c40513700cbbc24e3ea7b9747e18cbd
Bairoking <gs8004rtj5jj5j@gmail.com>
2023-05-17 21:49:27 +0000

    Task 3

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Componeent Tests', () => {
+  it('Renders without crashing', () => {
+    const app = shallow(<App />);
+    expect(app).toBeDefined();
+  });
+	it('renders a div with the class App-header', () => {
+		const app = shallow(<App />);
+
+		expect(app.find('.App-header')).toBeDefined();
+	});
+	it('renders a div with the class App-body', () => {
+		const app = shallow(<App />);
+
+		expect(app.find('.App-body')).toBeDefined();
+	});
+	it('renders a div with the class App-footer', () => {
+		const app = shallow(<App />);
+
+		expect(app.find('.App-footer')).toBeDefined();
+	});
 });
