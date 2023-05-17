1533f8b2d61a74765d4c6e86c7fce74c9071602f
Bairoking <gs8004rtj5jj5j@gmail.com>
2023-05-17 21:35:04 +0000

    Task 2

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -2,3 +2,5 @@ export const getFullYear = () => new Date().getFullYear();
 
 export const getFooterCopy = (isIndex) =>
               isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+
+export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD";
